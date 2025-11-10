e9f67d546ff01a1335f38d4fcad94961a2ad0919
ss23if007-design <ss23if007@gmail.com>
2025-11-10 17:30:02 +0530

    Fix case of Footer.css import

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
-import "./footer.css";
+import "./Footer.css";
 import { Panel } from './Panel';
 
 export const Footer = () => {
